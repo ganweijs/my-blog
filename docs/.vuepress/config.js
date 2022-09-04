8dc045f4cf08a23c0d3448d1f0dfb8d1cacf52b7
ganwei <644142242@qq.com>
2022-09-04 23:14:48 +0800

    feat:deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,34 @@
 module.exports = {
-  title: 'blog',
-  description: '',
+  title: 'VuePress',
+  description: 'this is ganwei‘s blog',
+  theme: 'reco',
+  base: '/my-blog/',
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
   themeConfig: {
         nav: [
             { text: '首页', link: '/' },
-            { 
-                text: '', 
-                items: [
-                    { text: 'Github', link: 'https://github.com/ganweijs' }
+            { text: 'Github', link: 'https://github.com/ganweijs' },
+            // { 
+            //     text: '更多', 
+            //     items: [
+            //         { text: '', link: '' }
+            //     ]
+            // }
+        ],
+        sidebar: [
+            {
+                title: '第一章',
+                path: '/',
+                collapsable: true, // 不折叠
+                children: [
+                    { title: "js", path: "/" }
                 ]
             }
-        ]
-    }
+        ],
+        subSidebar: 'auto'
+    }   
 }
